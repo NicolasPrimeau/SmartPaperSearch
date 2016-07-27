25d20f9c6a7001249ce4bb81ad636ee0d06ba5d7
Nicolas Primeau <nicolas.primeau@larus.com>
2016-07-27 16:01:28 -0400

    small changes

--- utils/interesting.py
+++ utils/interesting.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 
 if __name__ == '__main__' and __package__ is None:
-    from os import sys, path
+    import os
     # __file__ should be defined in this case
-    PARENT_DIR = path.dirname(path.dirname(path.abspath(__file__)))
-    sys.path.append(PARENT_DIR)
+    PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    os.sys.path.append(PARENT_DIR)
 
 from utils import DatabaseDAO
+import sys
 
+full = len(sys.argv) == 2 and sys.argv[1] == "full"
 
 def main():
     cnt = 0
     for article in DatabaseDAO.get_interesting():
         print(str(cnt) + " -- " + article["title"])
         print()
+        if full:
+            print("Interest: " + str(article["interest"]))
+            print()
+            print(article["abstract"])
+            print()
+
         cnt += 1
 
 
