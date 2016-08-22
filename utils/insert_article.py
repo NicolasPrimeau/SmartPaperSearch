64f0fa916efca9d6e9664079b8b17db0e53abeb2
Nicolas Primeau <nicolas.primeau@larus.com>
2016-08-22 14:59:59 -0400

    Added the ability to have a file as argument for new papers

--- utils/insert_article.py
+++ utils/insert_article.py
@@ -9,19 +9,33 @@ if __name__ == '__main__' and __package__ is None:
 from utils import DatabaseDAO
 import sys
 
-if len(sys.argv) != 4:
-    print("3 Arguments, in this order: title, interest, abstract")
+if len(sys.argv) != 4 and len(sys.argv) != 2:
+    print("3 Arguments, in this order: title, interest, abstract, or give the path to a properly formatted file")
     sys.exit()
 
-title = sys.argv[1].lower()
-interest = int(sys.argv[2])
-abstract = sys.argv[3]
-category = None
-if len(sys.argv) == 5:
-    category = sys.argv[4].split(",")
+if len(sys.argv) == 4:
+    title = sys.argv[1].lower()
+    interest = int(sys.argv[2])
+    abstract = sys.argv[3]
+else:
+    lines = [line.strip() for line in open(sys.argv[1])]
+    buffer = list()
+    title = None
+    interest = 1
+    abstract = None
+    for line in lines:
+        if line is not None and line != "":
+            buffer.append(line)
+        elif title is None:
+                title = " ".join(buffer)
+                buffer.clear()
+    if abstract is None:
+        abstract = " ".join(buffer)
+        buffer.clear()
+    del buffer
 
 if interest not in (0, 1):
     print("Interest must be 0 or 1")
     sys.exit()
 
-DatabaseDAO.save_article_full(title, abstract, interest, category)
+DatabaseDAO.save_article_full(title, abstract, interest)
